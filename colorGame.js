9266f9ae796693b589f7565d91b7cd14c0a52238
Wojtek <C:\Users\Wojtek i Marta\Desktop\IT\The BAT!\Gowny katalog poczty programu The Bat!>
2018-06-20 23:31:48 +0200

    RGB color JS game complete

---
+++ colorGame.js
@@ -0,0 +1,113 @@
+let numSquares = 6;
+let colors = [];
+let pickedColor;
+let squares = document.querySelectorAll(".square");
+let colorDisplay = document.getElementById('colorDisplay');
+let messageDisplay = document.querySelector("#message");
+let header = document.querySelector("header");
+let resetButton = document.querySelector('#reset');
+let modeButtons = document.querySelectorAll('.mode');
+
+init();
+
+function init() {
+    setupModeButtons();
+    setupSquares();
+    reset();
+}
+
+function setupModeButtons() {
+    for (let i = 0; i < modeButtons.length; i++) {
+        modeButtons[i].addEventListener('click', function () {
+            modeButtons[0].classList.remove('selected');
+            modeButtons[1].classList.remove('selected');
+            this.classList.add('selected');
+            this.textContent === 'Easy' ? numSquares = 3 : numSquares = 6;
+            reset();
+        });
+    }
+}
+
+function setupSquares() {
+    for (let i = 0; i < squares.length; i++) {
+        //add click listeners to squares
+        squares[i].addEventListener('click', function () {
+            //grab color of clicked square
+            let clickedColor = this.style.backgroundColor;
+            //compare color to pickedColor
+            if (clickedColor === pickedColor) {
+                messageDisplay.textContent = "Correct!";
+                resetButton.textContent = "Play Again";
+                changeColors(clickedColor);
+                header.style.backgroundColor = clickedColor;
+            } else {
+                this.style.backgroundColor = "#333333";
+                messageDisplay.textContent = "Try Again!";
+            }
+        });
+    }
+}
+
+function reset() {
+    colors = generateRandomColors(numSquares);
+    //pick a new random color from array
+    pickedColor = pickColor();
+    //change colorDisplay to match pickedColor
+    colorDisplay.textContent = pickedColor;
+    messageDisplay.textContent = "";
+    resetButton.textContent = "New Colors";
+    //change colors of squares
+    for (let i = 0; i < squares.length; i++) {
+        if (colors[i]) {
+            squares[i].style.display = 'block';
+            squares[i].style.backgroundColor = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+    //change header color and message
+    header.style.backgroundColor = "#333";
+}
+
+resetButton.addEventListener('click', function () {
+    reset();
+});
+
+
+
+function changeColors(color) {
+    //loop through all squares
+    for (let i = 0; i < squares.length; i++) {
+        //change each color to match given color
+        squares[i].style.backgroundColor = color;
+    }
+
+}
+
+function pickColor() {
+    let random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
+
+function generateRandomColors(num) {
+    //make an array
+    let arr = [];
+    //repeat num times
+    for (let i = 0; i < num; i++) {
+        //get random color and push into arr
+        arr.push(randomColor());
+    }
+    //return that array
+    return arr;
+}
+
+function randomColor() {
+    //pick a "red" from 0 - 255
+    let r = Math.floor(Math.random() * 256);
+    //pick a "green" from 0 - 255
+    let g = Math.floor(Math.random() * 256);
+    //pick a "blue" from 0 - 255
+    let b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r}, ${g}, ${b})`;
+}
